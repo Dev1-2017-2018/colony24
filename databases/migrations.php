b640cca5f1da5340ca47b8db6f9358f3a0cafafc
Yannis <yannis1403@hotmail.fr>
2017-11-29 15:56:51 +0100

    Composer -> Faker

--- databases/migrations.php
+++ databases/migrations.php
@@ -5,3 +5,6 @@ $default = [
 	PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
 	PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC
 ];
+
+$pdo = new PDO('mysql:host=DB_HOST;dbname=DB_DBNAME', 'DB_USER', 'DB_PASSWORD', $defaults);
+

f57888dadc5172f5b390d42ee0c41f4ac9e2a83a
“Yacine <yacine.fethi41@gmail.com>
2017-12-01 16:53:33 +0100

    Inscription/Connexion

--- views/front/auth.php
+++ views/front/auth.php
@@ -1,1 +1,14 @@
-auth.php
+<?php ob_start() ; ?>
+	<div class="form">
+		<form action="/auth" method="POST">
+		    <?php echo getFlashMessage() ; ?>
+		    <p><input type="email" name="email" placeholder="Email"></p>
+		    <p><input type="password" name="password" placeholder="Password"></p>
+		    <p><input type="submit" name="Connexion" value="Connexion"></p>
+		    <input type="hidden" name="token" value="<?php echo md5( date('Y-m-d h:i:00') . SALT ); ?>">
+		</form>
+		<a href="/add_user">ADD USER</a>
+	</div>
+<?php $content = ob_get_clean() ; ?>
+
+<?php include __DIR__ . '/../layouts/master.php' ?>

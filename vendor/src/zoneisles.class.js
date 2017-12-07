b5728f134eeadaceb892e403efd0a3b121d9c374
Vlad412 <vlad_pro@live.fr>
2017-12-07 16:35:09 +0100

    update

---
+++ vendor/src/zoneisles.class.js
@@ -0,0 +1,8 @@
+export default class zoneISLES {
+    constructor(positionX, positionY, width, height) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+    }
+}
---
+++ vendor/src/zoneor.class.js
@@ -0,0 +1,8 @@
+export default class Goldzone {
+    constructor(positionX, positionY, width, height) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+    }
+}

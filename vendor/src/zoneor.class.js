89f320bf1da50e0d59ab391c6bc15ef05efd0b35
Efingbv <efingbv@gmail.com>
2017-12-07 20:35:22 +0100

    Delete zoneor.class.js

--- vendor/src/zoneor.class.js
+++
@@ -1,8 +0,0 @@
-export default class Goldzone {
-    constructor(positionX, positionY, width, height) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.width = width;
-        this.height = height;
-    }
-}

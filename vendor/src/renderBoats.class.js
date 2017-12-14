292aa3c2c5d0310ce947c43f91ce98fd0d88344c
Dylan <efingbv@gmail.com>
2017-12-14 17:54:05 +0100

    update boat equipment and inventory

--- vendor/src/renderBoats.class.js
+++ vendor/src/renderBoats.class.js
@@ -1,6 +1,6 @@
 export default class RenderBoats{
-	constructor() {
-	    this.name = 'renderBoats';
+    constructor() {
+        this.name = 'renderBoats';
     }
 
     createBoatsButton(boats){
@@ -9,7 +9,7 @@ export default class RenderBoats{
         let svgBoat = '<svg width="100%" height="'+window.innerHeight+'" id="svgBoats">';
         //  ----------------------------------------
         let coordinate = {
-            x : 100,
+            x : 500,
             y : 200
         }
         //  ----------------------------------------
@@ -18,14 +18,13 @@ export default class RenderBoats{
                 boatType.push(boats[boat].name);
                 svgBoat += '<rect x="'+coordinate.x+'" y="'+coordinate.y+'" width="100" height="100" style="fill:rgb(0,255,150);" id="'+boats[boat].name+'"/>';
                 // ---------------------------------
-                coordinate.x += 150;
+                coordinate.x += 150
                 // ---------------------------------
             }
         }
         svgBoat += '</svg>';
         $('#player-boats').prepend(svgBoat);
-
-        $('#svgBoats').find('rect').on('click',function(){
+        $('#svgBoats').find('rect').on('click', function(){
             let list = '<div id="boatList" style="background-color: rgba(0,250,125,0.5); position: absolute;width: 90%;height: 90%;left: 5%;top: 5%"><h1 id="echapBoat" style="position:absolute;right:2%;top:0px;">X</h1></div>';
             $('body').append(list);
             let id = this.id;
@@ -36,27 +35,154 @@ export default class RenderBoats{
                             <p style="width: 30%; margin: 0 auto">${boats[boat].name} x:${boats[boat].x} y:${boats[boat].y}</p>
                             <input style="width: 45%; margin: 0 auto" type="number" placeholder="x"/>
                             <input style="width: 45%; margin: 0 auto" type="number" placeholder="y"/>
-                            <input type="button" value="Move"/>
+                            <input type="button" id="move" value="Se déplacer"/>
+                            <input type="button" id="boatEquipment" value="Equipement" data-id="${boats[boat].id}"/>
                         </div>
                     </li>`);
-                    $(`li#li${boats[boat].id}`).on('click', `input[type='button']`, { that: boats[boat]}, function (e) {
+                    $(`li#li${boats[boat].id}`).on('click', `input[type='button']#move`, { that: boats[boat]}, function (e) {
 
                         let context = e.data.that;
-
-                        console.log(context);
 
                         let inputX = $(`#li${context.id} > div > input:nth-child(2)`).val();
                         let inputY = $(`#li${context.id} > div > input:nth-child(3)`).val();
 
                         if (inputX != 0 || inputY != 0){
-                            console.log()
                             context.movement(inputY, inputX);
                             $(`#li${context.id} > div > p`).html(`${context.name} x:${context.x} y:${context.y}`);
                         }
                     });
 
+                    // click sur le bouton equipement d'un bateau
+                    $(`li#li${boats[boat].id}`).on('click', `input[type='button']#boatEquipment`, { that: boats[boat]}, function (e) {
+                        let context = e.data.that;
+                        let parent = context.parent;
+                        let equipement = parent.inventory;
+                        let $eqt = $('ul#inventory2-model');
+                        let dataId = $(this).data("id");
+
+                        document.getElementById('popupEquipment').style.display = "block";
+                        document.getElementById("popUp").style.display = "grid";
+
+                        window.onclick = function(event) {
+                            if (event.target === document.getElementById('background')) {
+                                document.getElementById('popupEquipment').style.display = "none";
+                                document.getElementById("popUp").style.display = "none";
+                                $('ul#inventory2-model li').remove();
+                            }
+                        }
+
+                        // Vérification des Values
+                        for (let value in equipement) {
+                            $eqt.append(`<li id="${value}" data-id="${dataId}"></li>`);
+                            if (equipement.hasOwnProperty(value)) {
+                                if (equipement[value] != "id") {
+                                    let eqtProperty = "";
+                                    for (let carac in equipement[value]) {
+                                        if(equipement[value][carac] != "") {
+                                            if (carac != 'id' && carac != 'Nom'  && carac != 'Prix') {
+                                                eqtProperty += `<br/> ${carac} : ${equipement[value][carac]}`;
+                                            }
+                                        }
+                                    }
+                                    inventoryRender($eqt, value, eqtProperty);
+                                }
+                            }
+                        }
+                    });
+                    // $('.closeButton').on('click', function() {
+                    //   $(this).closest('.modal').css('display','none');
+                    //   $(this).closest('.popUp').css('display','none');
+                    // });
+
+                    function inventoryRender($eqt, value, eqtProperty){
+                        $eqt.children().last().append(`
+                            <p style="color:black;">
+                                ${value}
+                                ${eqtProperty}
+                            </p>
+                        `);
+                    }
                 }
             }
+            // Click sur un équipement de l'inventaire
+            $(`ul#inventory2-model`).on('click', 'li',{ that: boats[0]}, function (e) {
+                console.log(e.data.that);
+                let context = e.data.that;
+                let parent = context.parent;
+                let equipement = parent.inventory;
+                let equipementName = parent.inventory.Nom;
+                let $eqt = $('ul#boatEquipment-model');
+
+                let liId = $(this).attr('id');
+                let dataId = $(this).data('id');
+
+                // Vérification des Values
+                equipement = equipement[liId];
+
+                $eqt.append(`<li id="${liId}" data-id="${dataId}"></li>`);
+                let eqtProperty = "";
+                for (let carac in equipement) {
+                    if(equipement[carac] != "") {
+                        if (carac != 'id' && carac != 'Nom'  && carac != 'Prix') {
+                            eqtProperty += `<br/> ${carac} : ${equipement[carac]}`;
+                        }
+                    }
+                }
+                $(this).remove();
+                if (parent.boats[dataId].equipement) {
+                    parent.boats[dataId].equipement[liId] = equipement;
+                } else{
+                    parent.boats[dataId].equipement = {};
+                    parent.boats[dataId].equipement[liId] = equipement;
+                }
+
+                delete parent.inventory[liId];
+                parent.saveDataJson(parent);
+                inventoryRender($eqt, liId, eqtProperty);
+                // console.log(parent.inventory);
+            });
+
+            // Click sur un équipement équipé à notre bateau
+            $(`ul#boatEquipment-model`).on('click', 'li',{ that: boats[0]}, function (e) {
+
+                let liId = $(this).attr('id');
+                let dataId = $(this).data('id');
+
+                let context = e.data.that;
+                let parent = context.parent;
+                let equipements = parent.boats[dataId].equipement;
+                let equipementName = parent.inventory.Nom;
+                let $eqt = $('ul#inventory2-model');
+
+                // Vérification des Values
+                let equipement = equipements[liId];
+
+                $eqt.append(`<li id="${liId}" data-id="${dataId}"></li>`);
+                let eqtProperty = "";
+                for (let carac in equipement) {
+                    if(equipement[carac] != "") {
+                        if (carac != 'id' && carac != 'Nom'  && carac != 'Prix') {
+                            eqtProperty += `<br/> ${carac} : ${equipement[carac]}`;
+                        }
+                    }
+                }
+                $(this).remove();
+
+                parent.inventory[liId] = equipement;
+                delete equipements[liId];
+                parent.saveDataJson(parent);
+                inventoryRender($eqt, liId, eqtProperty);
+                // console.log(parent.inventory);
+            });
+            function inventoryRender($eqt, value, eqtProperty){
+                $eqt.children().last().append(`
+                    <p style="color:black;">
+                        ${value}
+                        ${eqtProperty}
+                    </p>
+                `);
+            }
+
             $('#echapBoat').on('click', function(){
                 this.closest('div').remove();
             })
